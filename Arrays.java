5033e6355b7367d0158ebdb56a8d1e9512f32100
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2025-12-27 13:41:25 +0530

    update

--- Arrays.java
+++ Arrays.java
@@ -33,3 +33,23 @@
 //         System.out.println("Sum of array elements: " + sum);
 //     }
 // }
+
+//3. Find the largest element in an array
+// import java.util.Scanner;
+// public class Arrays {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for(int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         int largest = arr[0];
+//         for(int i = 1; i < n; i++) {
+//             if(arr[i] > largest) {
+//                 largest = arr[i];
+//             }
+//         }
+//         System.out.println("Largest element in the array: " + largest);
+//     }
+// }
