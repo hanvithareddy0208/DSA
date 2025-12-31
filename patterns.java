60728e6bd34d97eccc52adbeff76385df97213fa
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2025-12-31 11:56:54 +0530

    update

--- patterns.java
+++ patterns.java
@@ -16,7 +16,7 @@
 //             System.out.println( );
 //         }
 //     }
-// }
+
 
 //2.right angle triangle
 // import java.util.Scanner;
@@ -36,29 +36,57 @@
 //     }
 // }
 
-//3.Star Pyramid Pattern
+
+
+//3.hollow rectabgle
 // import java.util.Scanner;
-// public class patterns {
+// public class patterns{
 //     public static void main(String[] args) {
 //         Scanner sc = new Scanner(System.in);
 //         int n = sc.nextInt();
-//         if (n == 0) {
-//             System.out.print("NULL");
-//             return;
-//         }
-//         for (int i = 1; i <= n; i++) {
-//             for (int j = 1; j <= i; j++) {
-//                 if (j == i)
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(i==1||j==1||i==n||j==n){
 //                     System.out.print("*");
-//                 else
-//                     System.out.print("* ");
+//                 }else{
+//                     System.out.print(" ");
+//                 }
 //             }
 //             System.out.println();
 //         }
 //     }
 // }
 
-//import java.util
-kueghfiukhjbi;oek
-uiuebgoiko;lhkdd
-uiegfoih;oidck
+//4.inverted right angle triangle
+// import java.util.Scanner;
+// public class patterns{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=n;j>=i;j--){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//5.inverted right angle
+import java.util.Scanner;
+public class patterns{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+
