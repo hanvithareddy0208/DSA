bdfd88fde300d5b22f0102254358e32bd17e615c
hanvithareddy0208 <hanvithareddy0208@gmail.com>
2026-01-01 20:58:27 +0530

    update

--- Basics.java
+++ Basics.java
@@ -519,3 +519,25 @@
 //         System.out.println(area);
 //     }
 // }
+
+
+//30.echo program
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int k = sc.nextInt();
+//         System.out.println(n+" "+k);
+//         int arr[] = new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.print(arr[i]);
+//             if(i<n-1){
+//                 System.out.print(" ");
+//             }
+//         }
+//     }
+// }
